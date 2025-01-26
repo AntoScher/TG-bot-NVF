e70d49287282fb56b460091527fe27c1a90dd2d9
AntoScher <asc8er@gmail.com>
2025-01-26 11:16:40 +0300

    1

--- test_text.py
+++
@@ -1,4 +0,0 @@
-# Создание большого текстового файла для тестирования
-with open('input.txt', 'w', encoding='utf-8') as file:
-    for i in range(1000):  # 1000 строк
-        file.write(f"Это строка номер {i}. " * 10 + "\n")
